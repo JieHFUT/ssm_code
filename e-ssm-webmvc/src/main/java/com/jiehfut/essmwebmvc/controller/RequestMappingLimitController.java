57e6f8cac57e1bd6bb6457583ce26caab42235a9
JieCHERY <9594523wrj@gmail.com>
2025-01-11 14:47:28 +0800

    spring webmvc

--- e-ssm-webmvc/src/main/java/com/jiehfut/essmwebmvc/controller/RequestMappingLimitController.java
+++ e-ssm-webmvc/src/main/java/com/jiehfut/essmwebmvc/controller/RequestMappingLimitController.java
@@ -16,10 +16,13 @@ public class RequestMappingLimitController {
       @Reflective(ControllerMappingReflectiveProcessor.class)
       public @interface RequestMapping {
       String name() default "";
+
       @AliasFor("path")
       String[] value() default {};
+
       @AliasFor("value")
       String[] path() default {};
+
       RequestMethod[] method() default {};
       String[] params() default {};
       String[] headers() default {};
@@ -32,16 +35,43 @@ public class RequestMappingLimitController {
       请求方式：method => GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
       请求参数：params
       请求头：headers
-      请求内容类型：consumes
+      请求内容类型：consumes，浏览器必需携带什么格式的数据（MediaType）
+            表单在发送请求的时候：1.multipart/form-data - 文件上传（）
+                             2.application/x-www-form-urlencoded - 普通的 key-value 上传（）
+                             3.text/plain -
+            其他格式：4.application/json
+                    5.application/xml
+                    6.application/yaml
+                    7.image/png
+                    8.text/plain;charset=utf-8
+                    9.text/html;charset=utf-8
       响应内容类型：produces
-     */
+    */
 
-    @RequestMapping(value = "/limit-method", method = RequestMethod.GET)
+    @RequestMapping(value = "/limit-method", method = {RequestMethod.GET, RequestMethod.POST})
     public String limitMethod() {
         return "limit-method";
     }
 
+    @RequestMapping(value = "/param-limt", params = {"username", "age!=18", "!password"})
+    public String paramLimit(String username, Integer age) {
+        return "param-limt";
+    }
 
+    @RequestMapping(value = "/header-limit", headers = {"keya", "!keyb", "keyc=valuec"})
+    public String headerLimit() {
+        return "header-limit";
+    }
+
+    @RequestMapping(value = "/consumes-limit", consumes = {"application/json"})
+    public String consumesLimit() {
+        return "consumes-limit";
+    }
+
+    @RequestMapping(value = "/produces-limit", produces = {"text/plain;charset=utf-8"})
+    public String producesLimit() {
+        return "produces-limit";
+    }
 
 
 }
