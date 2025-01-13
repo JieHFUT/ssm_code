f94e170ff0248968d61344db5d1e1ca9818a0a5f
JieCHERY <9594523wrj@gmail.com>
2025-01-13 22:58:01 +0800

    spring mybatis

--- i-ssm-mybatis-example/src/test/java/com/jiehfut/issmmybatisexample/ISsmMybatisExampleApplicationTests.java
+++ i-ssm-mybatis-example/src/test/java/com/jiehfut/issmmybatisexample/ISsmMybatisExampleApplicationTests.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ISsmMybatisExampleApplicationTests {
 
-
     @Autowired
     EmpMapper empMapper;
 
@@ -22,6 +21,27 @@ class ISsmMybatisExampleApplicationTests {
          */
         Emp emp = empMapper.getEmpById(1);
         System.out.println(emp);
+
+        empMapper.insertEmp(new Emp(null, "zhansgan", 19, 23.34));
+
+        empMapper.updateEmp(new Emp(5, "lisi", 19, 23.34));
+
+        empMapper.deleteEmp(5);
     }
+
+
+    /**
+     * 设置主键回显
+     * .xml 文件设置
+     */
+    @Test
+    public void testPrimaryIdFeedback(){
+        Emp zhangsan = new Emp(null, "zhansgan", 19, 23.34);
+        empMapper.insertEmp(zhangsan);
+        System.out.println("id = " + zhangsan.getId()); // id = 5
+    }
+
+
+
 
 }
