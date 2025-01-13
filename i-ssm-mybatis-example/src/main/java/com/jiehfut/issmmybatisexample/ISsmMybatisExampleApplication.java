e7d52707a6b281c81007073ff3b916cab193a0cd
JieCHERY <9594523wrj@gmail.com>
2025-01-13 22:15:51 +0800

    spring webmvc

---
+++ i-ssm-mybatis-example/src/main/java/com/jiehfut/issmmybatisexample/ISsmMybatisExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jiehfut.issmmybatisexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISsmMybatisExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ISsmMybatisExampleApplication.class, args);
+    }
+
+}
---
+++ i-ssm-mybatis-example/src/test/java/com/jiehfut/issmmybatisexample/ISsmMybatisExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiehfut.issmmybatisexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ISsmMybatisExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
